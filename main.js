d25fbba224d7d44a893241fb86cb8e6dabf687e2
Kyle <kylejordanwilliams@gmail.com>
2023-04-10 11:09:57 +0100

    Kata ready

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-/* 
-👉 Write your kata here!
-*/
+/* 👉 Write your kata here!*/
+
+/*Description:
+Your goal is to write a function called reverseString that takes a str as a parameter and
+returns a new string with the characters in REVERSE order. Hip hip arrays!
+
+Here's a few Examples:
+Tester(reverseString("omg"), "gmo");
+Tester(reverseString("why"), "yhw");*/
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function reverseString(str) {
+  // Your code here
+}
+
+// Sample Test Cases
+console.log(reverseString("hello")); // Should output 'olleh'
+console.log(reverseString("12345")); // Should output '54321'
--- main.test.js
+++ main.test.js
@@ -1,1 +1,51 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import { reverseString } from "./main.js";
+
+/*test("reverseString", () => {
+  const str = "hello";
+  const str2 = "12345";
+  const actual1 = reverseString(str);
+  const actual2 = reverseString(str2);
+  const expected1 = "olleh";
+  const expected2 = "54321";
+  expect(actual1).toBe(expected1);
+  expect(actual2).toBe(expected2);
+ 
+});*/
+
+test("reverseString1", () => {
+  const str = "hello";
+  /* Define a constant variable named `str` 
+  and initialize it to the string "hello"*/
+  const actual = reverseString(str);
+  /* Call the `reverseString()` function on the `str` 
+  variable and store the result in a constant variable named `actual`*/
+  const expected = "olleh";
+  /* Define a constant variable named `expected` and 
+  initialize it to the string "olleh"*/
+  expect(actual).toBe(expected);
+  /* Use the Jest `expect()` function to test whether `actual` 
+  is equal to `expected`*/
+});
+
+test("reverseString2", () => {
+  const str = "12345";
+  const actual = reverseString(str);
+  const expected = "54321";
+  expect(actual).toBe(expected);
+});
+
+test("reverseString3", () => {
+  const str = "bobcat";
+  const actual = reverseString(str);
+  const expected = "tacbob";
+  expect(actual).toBe(expected);
+});
+
+test("reverseString4", () => {
+  const str = "kyle";
+  const actual = reverseString(str);
+  const expected = "elyk";
+  expect(actual).toBe(expected);
+});
